2418db2b475b78b6dfa38cc9fc28b2395a3a0039
codepologist <ombu.oneal@yahoo.com>
2025-02-21 13:26:21 +0100

    Modified the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ const Footer = () => {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          <strong>Bulma</strong> by
           <a href="https://book-worm-beta.vercel.app/">BookWorm</a>. The source
           code is Open Source
           <a href="https://github.com/codepologist/BookWorm.git">GIT</a>. The
