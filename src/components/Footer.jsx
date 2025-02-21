1ea0073ce8953b9a5943a8a1873ec652108c5085
codepologist <ombu.oneal@yahoo.com>
2025-02-21 13:40:00 +0100

    Modified Footer component copyrights information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,14 @@ const Footer = () => {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          <a href="https://book-worm-beta.vercel.app/">&copy; BookWorm 2025</a>.
-          The source code is Open Source and available at{" "}
-          <a href="https://github.com/codepologist/BookWorm.git">GitHub</a>
+          <a href="https://book-worm-beta.vercel.app/">&copy; 2025 BookWorm</a>.
+          The source code is Open Source and available at
+          <a
+            href="https://github.com/codepologist/BookWorm.git"
+            target="_blank"
+          >
+            GitHub
+          </a>
         </p>
       </div>
     </footer>
