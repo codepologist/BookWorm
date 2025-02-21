a9abff503c582a4d655f89a4430b2ca0b2b5085d
codepologist <ombu.oneal@yahoo.com>
2025-02-21 13:23:29 +0100

    Created the Footer component file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>Bulma</strong> by
+          <a href="https://book-worm-beta.vercel.app/">BookWorm</a>. The source
+          code is Open Source
+          <a href="https://github.com/codepologist/BookWorm.git">GIT</a>. The
+          website content is OpenSource .
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
